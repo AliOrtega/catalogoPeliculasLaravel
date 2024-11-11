3b5059e68e1550476ccdd7a67481b2e0f0eb7323
Alis <bertha.ortega@udgvirtual.udg.mx>
2024-11-11 13:44:26 -0600

    Añadido ReactPHP para iniciar el servidor en Laravel y actualizaciones en composer.json

--- server.php
+++ server.php
@@ -1,10 +1,28 @@
 <?php
+
+require __DIR__ . '/vendor/autoload.php';
+
 $port = getenv('PORT') ?: 8000;
 
-$app = require_once __DIR__.'/public/index.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
 $httpHost = '0.0.0.0:' . $port;
-$httpApp = new React\Http\HttpServer($app);
-$httpApp->listen(new React\Socket\Server($httpHost));
+$httpApp = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) use ($kernel) {
+    $request = \Illuminate\Http\Request::createFromBase($request);
+    $response = $kernel->handle($request);
+
+    return new React\Http\Message\Response(
+        $response->getStatusCode(),
+        $response->headers->all(),
+        $response->getContent()
+    );
+});
+
+$socket = new React\Socket\Server($httpHost, $loop);
+$httpApp->listen($socket);
 
 echo "Laravel development server started on http://localhost:$port\n";
+
+$loop->run();
