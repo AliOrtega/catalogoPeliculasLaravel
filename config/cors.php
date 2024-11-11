2ed280795668ab31f1bdb476f51cc99947011707
Alis <bertha.ortega@udgvirtual.udg.mx>
2024-11-11 16:06:41 -0600

    Actualizar configuración de CORS para permitir solicitudes desde el frontend Angular en GitHub Pages

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,8 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://aliortega.github.io'],
+    'allowed_origins' => ['https://aliortega.github.io/catalogoPeliculasAngular/'],
+    'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
