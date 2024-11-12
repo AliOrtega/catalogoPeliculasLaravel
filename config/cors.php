edfe996e41ed8a294aa36de171cf7ce50b1d6a4c
Alis <bertha.ortega@udgvirtual.udg.mx>
2024-11-11 18:31:43 -0600

    Update CORS configuration to allow GitHub Pages frontend

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://aliortega.github.io/catalogoPeliculasAngular/'],
+    'allowed_origins' => ['https://aliortega.github.io'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
